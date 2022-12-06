6c90d1965abc4bf0cb8f4c400460feaf83c53675
Simply Noel <81578870+NoieeTheBeginnerUwU@users.noreply.github.com>
2022-12-06 20:47:40 +0800

    a

--- assets/js/calendar/demo.js
+++ assets/js/calendar/demo.js
@@ -5,33 +5,7 @@ $(document).ready( function(){
 
 	theDays = ["S", "M", "T", "W", "T", "F", "S"];
     events = [
-      [
-        "4/"+month+"/"+year, 
-        'Meet a friend', 
-        '#', 
-        '#177bbb', 
-        'Contents here'
-      ],
-      [
-        "7/"+month+"/"+year, 
-        'Kick off meeting!', 
-        '#', 
-        '#1bbacc', 
-        'Have a kick off meeting with .inc company'
-      ],
-      [
-        "17/"+month+"/"+year, 
-        'Milestone release', 
-        '#', 
-        '#fcc633', 
-        'Contents here'
-      ],
-      [
-        "19/"+month+"/"+year, 
-        'A link', 
-        'http://www.google.com', 
-        '#e33244'
-      ]
+      
     ];
     $('#calendar').calendar({
         months: theMonths,
